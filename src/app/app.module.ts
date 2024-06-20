884d8b7deb8dd1d8cbd07ebcab80071cb6333d9d
Trachinus <Trachinusdracox@gmail.com>
2024-06-20 10:56:21 +0200

    inicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmpleadoComponent,
+    EmpleadosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
